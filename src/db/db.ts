2c45d703521ddf96dd587d52cfeca3bdb0ab8525
Simon Escudero <as.escudero@alumno.etec.um.edu.ar>
2023-11-26 14:24:39 -0300

    base de datos con prodcutos

--- src/db/db.ts
+++ src/db/db.ts
@@ -15,9 +15,18 @@ export const AppDataSource = new DataSource({
 })
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("La base anda")
-    })
+    .then(async() => {
+        const productRepository = AppDataSource.manager.getRepository(Product);
+        const existingProducts = await productRepository.find();
+        if (existingProducts.length === 0) {
+          const producto1 = new Product("Press De Banca", 900, "https://cdn.ossfitness.com/img/full/ASDC41%20-%20Etenon%20Press%20de%20Banca%20Am%20SS.jpg");
+          const producto2 = new Product("Press Inclinado", 950, "https://jbs-fitness.es/wp-content/uploads/2020/07/PRESS-BANCA-PECHO-INCLINADO-B-020-2-scaled.jpg");
+          const producto3 = new Product("Press Declinado", 750, "https://jbh.com.ar/wp-content/uploads/2020/11/banco-declinado-gold-2-scaled.jpg");
+
+          await productRepository.save([producto1, producto2, producto3]);
+          console.log("La base anda");
+    }})
+
     .catch((error) => {
         console.log(error)
 
